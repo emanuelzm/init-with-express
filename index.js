beaa80392f55a0d9903e88206cb4e6f7d7615670
Emanuel Zabaleta <emanuelzm0220@gmail.com>
2023-04-10 02:41:17 -0500

    Middlewares

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+//Middlewares
 
-app.all("/user", (req, res, next) => {
+function logger(req, res, next) {
+    console.log(`Route received: ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+    next();
+}
+
+app.use(express.json());
+app.use(logger); // Para que se valide info antes de llegar a las rutas
+
+/*app.all("/user", (req, res, next) => {
     console.log("Por aquí paso");
     next();
-});
+});*/
 
 app.get("/user", (req, res) => {
     //res.send("Petición GET recibida");
