78a482cdde1be141ca45ecb5592e7e53f8f8703a
Emanuel Zabaleta <emanuelzm0220@gmail.com>
2023-04-10 02:31:45 -0500

    Rutas & Rutas dinamicas en Express

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Petición GET recibida");
+app.use(express.json());
+
+app.all("/user", (req, res, next) => {
+    console.log("Por aquí paso");
+    next();
 });
 
-app.post("/about", (req, res) => {
+app.get("/user", (req, res) => {
+    //res.send("Petición GET recibida");
+    res.json({
+        username: "Cameron",
+        lastname: "Howe"
+    });
+});
+
+app.post("/user/:id", (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
     res.send("Petición POST recibida");
 });
 
-app.put("/contact", (req, res) => {
-    res.send("Petición PUT recibida");
+app.put("/user/:id", (req, res) => {
+    console.log(req.body);
+    res.send(`User ${req.params.id} updated`);
 });
 
-app.delete("/test", (req, res) => {
-    res.send("<h1>Petición DELETE recibida</h1>");
+app.delete("/user/:userId", (req, res) => {
+    res.send(`User ${req.params.userId} deleted`);
 });
 
 app.listen(3000, () => {
