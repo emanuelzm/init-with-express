31dfe763b9c15bafacd85f1d2b7bbc0987600fc0
Emanuel Zabaleta <emanuelzm0220@gmail.com>
2023-04-10 02:11:56 -0500

    About the http methods

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Petición GET recibida");
+});
+
+app.post("/about", (req, res) => {
+    res.send("Petición POST recibida");
+});
+
+app.put("/contact", (req, res) => {
+    res.send("Petición PUT recibida");
+});
+
+app.delete("/test", (req, res) => {
+    res.send("<h1>Petición DELETE recibida</h1>");
 });
 
 app.listen(3000, () => {
-    console.log("Serve on port 3000");
+    console.log("Serve on port 5000");
 });
